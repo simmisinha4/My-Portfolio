4c4fd79a074b0d1b2de2080fe4e1513ed05e0f37
simmisinha4 <183376960+simmisinha4@users.noreply.github.com>
2025-08-17 23:10:19 +0530

    change the resume link

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -243,7 +243,7 @@ const profile = {
 
                 {/* Contact Button */}
                 <a
-                  href="https://drive.google.com/file/d/100fdbqiOytG8eJ_RWzIj4Tiw9eHVoF_d/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1Yl7F0p0micasOSWAI3ukRd-iInninLVW/view?usp=sharing"
                   className="group relative inline-flex items-center justify-center gap-3 p-0.5 rounded-xl bg-gradient-to-r from-gray-800 to-gray-700 transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
                 >
                   <span className="block w-full px-6 sm:px-8 py-3 sm:py-4 rounded-[11px] bg-gray-900 border border-gray-700/50 transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-gray-800 group-hover:to-gray-700">
